eef608be751af67d5ff4db73077d4ffc947880d7
Denis Ivanchenko <sicknature24@yandex.com>
2023-04-25 14:55:04 +0300

    refactor: apply eslint to index.js file; rename properties in style.css file

--- src/index.js
+++ src/index.js
@@ -1,38 +1,77 @@
 const englishButtons = [
-    "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
-    "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del",
-    "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "Enter",
-    "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "▲", "ShR",
-    "Ctrl", "Win", "Alt", "Space", "Alt", "◄", "▼", "►"
+  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'Enter',
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'ShR',
+  'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►',
 ];
 
-const keyboardKeysContainer = document.querySelector('.keyboard-keys-container');
+// todo: подумай насчет деструктуризации и классов
 
-englishButtons.forEach((key) => {
-    const newButton = document.createElement("button");
-    newButton.classList.add("keyboard-button");
+const body = document.querySelector('body');
+
+const generatePageLayout = () => {
+  const keyboardWrapper = document.createElement('div');
+  keyboardWrapper.classList.add('keyboard-wrapper');
+
+  const keyboardTitle = document.createElement('h1');
+  keyboardTitle.textContent = 'Virtual Keyboard EN/RU';
+  keyboardTitle.classList.add('keyboard-title');
+
+  const keyboardTextArea = document.createElement('textarea');
+  keyboardTextArea.classList.add('keyboard-textarea');
+
+  const keyboardKeysContainer = document.createElement('div');
+  keyboardKeysContainer.classList.add('keyboard-keys-container');
+
+  const keyboardDescription = document.createElement('p');
+  keyboardDescription.classList.add('keyboard-description');
+  keyboardDescription.textContent = 'Клавиатура создана в операционной системе Windows '
+        + 'Для переключения языка комбинация: ctrl + alt';
+
+  keyboardWrapper.appendChild(keyboardTitle);
+  keyboardWrapper.appendChild(keyboardTextArea);
+  keyboardWrapper.appendChild(keyboardKeysContainer);
+  keyboardWrapper.appendChild(keyboardDescription);
+
+  body.appendChild(keyboardWrapper);
+};
+
+const generateKeyboardKeys = () => {
+  const keyboardKeysContainer = document.querySelector('.keyboard-keys-container');
+
+  englishButtons.forEach((key) => {
+    const newButton = document.createElement('button');
+    newButton.classList.add('keyboard-button');
     newButton.textContent = key;
     keyboardKeysContainer.appendChild(newButton);
 
     switch (key) {
-        case "Backspace":
-            newButton.classList.add("keyboard-mx-button");
-            break;
-        case "Tab":
-        case "Del":
-            newButton.classList.add("keyboard-sx-button");
-            break;
-        case "Space":
-            newButton.classList.add("keyboard-space-button");
-            break;
-        case "CapsLock":
-            newButton.classList.add("keyboard-shift-button");
-            break;
-        case "Enter":
-            newButton.classList.add("keyboard-enter-button");
-            break;
-        case "Shift":
-            newButton.classList.add("keyboard-shift2-button");
-            break;
+      case 'Backspace':
+        newButton.classList.add('button-backspace');
+        break;
+      case 'Tab':
+      case 'Del':
+        newButton.classList.add('keyboard-sx-button');
+        break;
+      case 'Space':
+        newButton.classList.add('button-space');
+        break;
+      case 'CapsLock':
+        newButton.classList.add('button-capslock');
+        break;
+      case 'Enter':
+        newButton.classList.add('button-enter');
+        break;
+      case 'Shift':
+        newButton.classList.add('button-shift');
+        break;
+      default:
     }
-})
+  });
+};
+
+(function initPage() {
+  generatePageLayout();
+  generateKeyboardKeys();
+}());
